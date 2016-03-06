77998c0170e1486cfb50f433103ac8ab7c07ab7e
Saz <s4saz@hotmail.com>
2016-03-06 14:46:23 +0000

    this should ve been added with the previous commit

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -1,8 +1,11 @@
 package controllers
 
-/**
-  * Created by s4saz_000 on 06/03/2016.
-  */
-class Products {
+import play.api.mvc.{Action, Controller}
+import models.Product
 
+object Products extends Controller {
+  def list = Action { implicit request =>
+    val products = Product.findAll
+    Ok(views.html.products.list(products))
+  }
 }
